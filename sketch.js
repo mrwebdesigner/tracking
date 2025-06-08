f797928d1f9f4a1bedcc397808b26c289a3036ba
mariarumori <rumorimaria@gmail.com>
2025-06-08 18:24:14 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,26 @@
 // @ts-nocheck
 let video;
-
-/** @type {ml5.HandPose} */
 let handPose;
-
-/** @type {ml5.Hand[]} */
 let hands = [];
-
-/** @type {ml5.faceMesh} */
 let faceMesh;
-
-/** @type {ml5.faces[]} */
 let faces = [];
-
 let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
 
-let spostamento = {
-  x: 10,
-  y: 10,
-};
+let rotationAngle = 0;
 
 function preload() {
-  // Load the handPose model
-  handPose = ml5.handPose({
-    flipped: true,
-  });
-
+  handPose = ml5.handPose({ flipped: true });
   faceMesh = ml5.faceMesh(options);
 }
 
 function setup() {
-  const scale = 2;
-  createCanvas(640 * scale, 480 * scale);
+  createCanvas(640, 480, WEBGL); // WEBGL necessario per rotazione 3D
 
-  // Create the webcam video and hide it
-  video = createCapture(VIDEO, { flipped: false });
+  video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 
-  // start detecting hands from the webcam video
-  handPose.detectStart(video, function (results) {
+  handPose.detectStart(video, (results) => {
     hands = results;
   });
 
@@ -47,78 +28,62 @@ function setup() {
 }
 
 function draw() {
-  // Draw the webcam video
+  background(0);
+
+  // Mostra il video come texture generale dietro
+  push();
+  translate(-width / 2, -height / 2);
   image(video, 0, 0, width, height);
+  pop();
 
-  //
+  let rotazione = 0;
 
-  const mano_1 = hands[0];
-  const mano_2 = hands[1];
-
-  if (mano_1 && mano_2) {
-    const pollice_1 = mano_1.keypoints[4];
-    const indice_1 = mano_1.keypoints[8];
-
-    const distanza_mano_1 = dist(
-      pollice_1.x,
-      pollice_1.y,
-      indice_1.x,
-      indice_1.y
-    );
-
-    const pollice_2 = mano_2.keypoints[4];
-    const indice_2 = mano_2.keypoints[8];
-
-    const distanza_mano_2 = dist(
-      pollice_2.x,
-      pollice_2.y,
-      indice_2.x,
-      indice_2.y
-    );
-
-    spostamento.x = distanza_mano_1;
-    spostamento.y = distanza_mano_2;
+  // Calcola rotazione in base alla distanza dita mano 1
+  if (hands[0]) {
+    let pollice = hands[0].keypoints[4];
+    let indice = hands[0].keypoints[8];
+    let d = dist(pollice.x, pollice.y, indice.x, indice.y);
+    rotazione = map(d, 0, 150, 0, PI); // rotazione Y
   }
 
+  // Disegna griglia rotante sopra al volto
   for (let i = 0; i < faces.length; i++) {
-    let face = faces[i];
-    console.log(face);
-    let box = face.box;
-    push();
-    beginClip();
-    rect(box.xMin, box.yMin, box.width, box.height);
-    endClip();
-    image(video, spostamento.x, spostamento.y, width, height);
-    pop();
+    let box = faces[i].box;
+    drawFaceGridWithRotation(box, rotazione);
   }
-
-  // strokeWeight(4);
-
-  // const mano_1 = hands[0];
-  // if (!mano_1) return;
-
-  // const mano_1_pollice = mano_1.keypoints[4];
-  // const mano_1_indice = mano_1.keypoints[8];
-
-  // stroke(255, 0, 0);
-  // line(mano_1_pollice.x, mano_1_pollice.y, mano_1_indice.x, mano_1_indice.y);
-
-  // const mano_2 = hands[1];
-  // if (!mano_2) return;
-
-  // const mano_2_pollice = mano_2.keypoints[4];
-  // const mano_2_indice = mano_2.keypoints[8];
-
-  // stroke(0, 0, 255);
-  // line(mano_2_pollice.x, mano_2_pollice.y, mano_2_indice.x, mano_2_indice.y);
-
-  // stroke(255, 255, 0);
-  // line(mano_1_pollice.x, mano_1_pollice.y, mano_2_pollice.x, mano_2_pollice.y);
-
-  // stroke(0, 255, 0);
-  // line(mano_1_indice.x, mano_1_indice.y, mano_2_indice.x, mano_2_indice.y);
 }
+
+// Disegna griglia di celle che ruotano
+function drawFaceGridWithRotation(box, rotazione) {
+  let cols = 10;
+  let rows = 10;
+  let cellW = box.width / cols;
+  let cellH = box.height / rows;
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let px = box.xMin + x * cellW;
+      let py = box.yMin + y * cellH;
+
+      let cx = px + cellW / 2 - width / 2;
+      let cy = py + cellH / 2 - height / 2;
+
+      push();
+      translate(cx, cy, 0);
+
+      // Rotazione Y dinamica per ogni cella
+      let localPhase = sin(x + y + frameCount * 0.05);
+      rotateY(rotazione * localPhase);
+
+      // Mappa il video sul rettangolo
+      texture(video);
+      noStroke();
+      plane(cellW, cellH);
+      pop();
+    }
+  }
+}
+
 function gotFaces(results) {
-  // Save the output to the faces variable
   faces = results;
 }
