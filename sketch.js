04e2af7f7832b2a9f5f91bd59fa338ba73ee5b1b
mariarumori <rumorimaria@gmail.com>
2025-06-18 16:02:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,15 +4,19 @@ let handPose;
 let hands = [];
 let faceMesh;
 let faces = [];
-let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
-let sfasamento = 20;
+let sfasamento = 1;
 let scaleFactor = 1;
-
-//let rotationAngle = 0;
+let grid_rows = 20;
+let grid_cols = 20;
+let grid_center = createVector(width / 2, height / 2);
 
 function preload() {
   handPose = ml5.handPose({ flipped: false });
-  faceMesh = ml5.faceMesh(options);
+  faceMesh = ml5.faceMesh({
+    maxFaces: 1,
+    refineLandmarks: false,
+    flipHorizontal: false,
+  });
 }
 
 function setup() {
@@ -31,33 +35,29 @@ function setup() {
 
 function draw() {
   background(0);
-  //rotationAngle += 0.01;
 
   // Mostra il video come texture generale dietro
 
   image(video, 0, 0, width, height);
 
-  // Disegna griglia rotante sopra al volto
   for (let i = 0; i < faces.length; i++) {
     let box = faces[i].box;
     drawUniqueVideoGridOnFace(faces[i], 5, 6);
   }
 
-  //sfasamento = mouseX;
   if (hands.length > 0) {
     let hand = hands[0];
     let indexTip = hand.keypoints.find((p) => p.name === "index_finger_tip");
     if (indexTip) {
       // Mappa la coordinata X sul canvas
-      sfasamento = map(indexTip.x, 0, video.width, 0, 100);
-      //rotationAngle = map(indexTip.y, 0, video.height, -PI / 4, PI / 4);
+      sfasamento = map(indexTip.x, 0, video.width, 0.5, 2);
       scaleFactor = map(indexTip.y, 0, video.height, 2, 0.5);
       scaleFactor = constrain(scaleFactor, 0.5, 2);
     }
   }
 }
 
-function drawUniqueVideoGridOnFace(face, cols = 5, rows = 6) {
+function drawUniqueVideoGridOnFace(face, cols = grid_cols, rows = grid_rows) {
   const keypoints = face.keypoints;
   // Coordinate viso nel video
   let left = face.box.xMin;
@@ -69,32 +69,54 @@ function drawUniqueVideoGridOnFace(face, cols = 5, rows = 6) {
   let baseCellW = boxWidth / cols;
   let baseCellH = boxHeight / rows;
 
-  let cellW = baseCellW * scaleFactor;
-  let cellH = baseCellH * scaleFactor;
+  let grid_center = createVector(left + boxWidth / 2, top + boxHeight / 2);
 
-  //let cellW = boxWidth / cols;
-  //let cellH = boxHeight / rows;
 
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      //let cx = face.box.xMin + cellW * x;
-      //let cy = face.box.yMin + cellH * y;
+  for (let y = 0; y < grid_cols; y++) {
+    for (let x = 0; x < grid_cols; x++) {
 
-      let cx = left + baseCellW * x;
+      let cx = left + grid_rows * x;
       let cy = top + baseCellH * y;
+
+      let cella_center = createVector(cx + baseCellW / 2, cy + baseCellH / 2);
+      let diff = p5.Vector.sub(cella_center, grid_center);
+
+      let sorgente_center = grid_center.add(diff.mult(sfasamento));
+      let sx = sorgente_center.x - baseCellW / 2;
+      let sy = sorgente_center.y - baseCellH / 2;
+
+      let cell_w = width / grid_cols;
+      let cell_h = height / grid_rows;
+      let cell_x = i * cell_w;
+      let cell_y = j * cell_h;
+
+      let cell_center = createVector(cell_x + cell_w / 2, cell_y + cell_h / 2);
+      
+      let diff = p5.Vector.sub(cell_center, grid_center)
+      let m = map(mouseX, 0, width, 0.5, 1.5)
+      let source_center = p5.Vector.add(grid_center, diff.mult(m))
+      
+      let source_x = source_center.x - cell_w/2
+      let source_y = source_center.y - cell_h/2
 
       copy(
         video,
-        cx - x * sfasamento,
-        cy - y * sfasamento,
-        cellW,
-        cellH,
-        cx - cellW / 2 + baseCellW / 2,
-        cy - cellH / 2 + baseCellH / 2,
-        //cx,
-        //cy,
-        cellW,
-        cellH
+        sx,
+        sy,
+        baseCellW,
+        baseCellH,
+        cx,
+        cy,
+        baseCellW,
+        baseCellH
+        source_x,
+        source_y,
+        cell_w,
+        cell_h,
+        cell_x,
+        cell_y,
+        cell_w,
+        cell_h
       );
     }
   }
@@ -103,39 +125,3 @@ function drawUniqueVideoGridOnFace(face, cols = 5, rows = 6) {
 function gotFaces(results) {
   faces = results;
 }
-
-//let rotazione = 0;
-
-// Calcola rotazione in base alla distanza dita mano 1
-//if (hands[0]) {
-//let pollice = hands[0].keypoints[4];
-//let indice = hands[0].keypoints[8];
-//let d = dist(pollice.x, pollice.y, indice.x, indice.y);
-//rotazione = map(d, 0, 150, 0, PI); // rotazione Y
-//}
-
-// Disegna griglia di celle che ruotano
-//function drawFaceGridWithRotation(box, rotazione) {
-//let cols = 4;
-//let rows = 4;
-//let cellW = box.width / cols;
-//let cellH = box.height / rows;
-
-//for (let y = 0; y < rows; y++) {
-//for (let x = 0; x < cols; x++) {
-//let px = box.xMin + x * cellW;
-//let py = box.yMin + y * cellH;
-
-//let cx = px + cellW / 2 - width / 2;
-//let cy = py + cellH / 2 - height / 2;
-
-//push();
-//translate(cx, cy, 0);
-
-// Rotazione Y dinamica per ogni cella
-//let localPhase = sin(x + y + frameCount * 0.05);
-//rotateY(rotazione * localPhase);
-
-//}
-//}
-//}
